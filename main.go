fde126430ee82ef4a17c926c616d607eaaf279a5
wawesomeNOGUI <wsforc3y@gmail.com>
2021-01-12 17:31:44 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -3,17 +3,56 @@ package main
 import(
   "time"
   "log"
+  "os"
+  "bufio"
+  "io"
+  "strconv"
+  "strings"
 )
 
+func MustReadStdin() string {
+  r := bufio.NewReader(os.Stdin)
+
+  var in string
+  for {
+    var err error
+    in, err = r.ReadString('\n')
+    if err != io.EOF {
+      if err != nil {
+        panic(err)
+      }
+    }
+    in = strings.TrimSpace(in)
+    if len(in) > 0 {
+      break
+    }
+  }
+  log.Println("")
+
+  return in
+}
+
 func main(){
+  log.Printf("How many minutes to Sleep?:")
+
+  x, err := strconv.Atoi( MustReadStdin() )
+  if err != nil {
+    panic(err)
+  }
+
   START:
-    time.Sleep(time.Minute*15)
+    for i := 0; i < x; i++{
+      time.Sleep(time.Minute)
+      log.Printf( strconv.Itoa(x) )
+    }
+
     log.Println("\a")
+    time.Sleep(time.Second)
     log.Println("\a")
-    log.Println("\a")
-    log.Println("\a")
+    time.Sleep(time.Second)
     log.Println("\a")
 
+    log.Printf("End of timer! One minute until the timer resetarts.")
     time.Sleep(time.Minute)
   goto START
 
